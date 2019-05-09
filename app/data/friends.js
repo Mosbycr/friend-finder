29d75e3d63a53691617d59b6cffa3f68f219e5d3
Mosbycr <mosbycr@gmail.com>
2019-05-08 21:25:35 -0400

    added entries to friends data base so there would already be some profiles to test against

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,16 @@
-var friends = []
+var friends = [
+  {
+    name: "Mike Reynolds",
+    photo:
+      "https://businessandfinance.com/wp-content/uploads/2017/12/Gene-Murtagh-Kingspan.jpg",
+    answers: ["5", "2", "1", "1", "3", "4", "3", "1", "2", "1"]
+  },
+  {
+    name: "Sarah Witley",
+    photo:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    answers: ["1", "3", "1", "4", "3", "3", "3", "1", "2", "1"]
+  }
+];
 
 module.exports = friends;
