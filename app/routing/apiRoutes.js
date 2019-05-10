91f277d6919101859b7947ac3a61ae5d17e2e09f
Mosbycr <mosbycr@gmail.com>
2019-05-10 15:51:18 -0400

    added friend match code in post in the apiRoutes.js, not yet tested

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,34 @@ module.exports = function(app){
 
     app.post("/api/friends", function(req, res) {
       var newProfile = req.body;
+      var currentProfile = req.body.answers;
+      console.log(currentProfile);
+      //max difference answer could be 4*10
+      var totalDiff = 40;
+      var bestFriend;
+
+      for(var i = 0; i < friends.length; i++){
+        var friendAnswers = friends[i].answers;
+        console.log(friendAnswers);
+        var difference = 0;
+
+        for(var j = 0; j < friendAnswers.length; j++){
+          difference += difference + Math.abs(friendAnswers[j] - currentProfile[j]);
+          console.log(difference);
+
+        }
+
+         if (difference <= totalDiff) {
+           totalDiff = difference;
+           console.log(totalDiff);
+           bestFriend = friends[i];
+           console.log(bestFriend);
+         }
+
+        
+      }
+
+
       friends.push(newProfile);
       //res.json(newFriend);
     });
