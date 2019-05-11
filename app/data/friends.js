ca7b092b35e1ca8748a2f04cc9846b9bc8f04a64
Mosbycr <mosbycr@gmail.com>
2019-05-10 22:24:37 -0400

    added photos for friends data that are 500 or smaller

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,19 @@
 var friends = [
   {
     name: "Mike Reynolds",
-    photo:
-      "https://businessandfinance.com/wp-content/uploads/2017/12/Gene-Murtagh-Kingspan.jpg",
+    photo: "https://img.rasset.ie/00119955-500.jpg",
     answers: ["5", "2", "1", "1", "3", "4", "3", "1", "2", "1"]
   },
   {
     name: "Sarah Witley",
     photo:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    answers: ["1", "3", "1", "4", "3", "3", "3", "1", "2", "1"]
+  },
+  {
+    name: "Thor God of Thunder",
+    photo:
+      "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/09/1519729389-thor-ragnarok-reviews-big.jpg?resize=480:*",
     answers: ["1", "3", "1", "4", "3", "3", "3", "1", "2", "1"]
   }
 ];
